c1c7700b4f3699c12f93f4ab294c84119f3d77a0
eko8757 <ekomurdiansyah89@gmail.com>
2019-11-20 23:26:56 +0700

    add menu

---
+++ app/src/main/java/com/dev/custommapbox/SplashScreen.kt
@@ -0,0 +1,36 @@
+package com.dev.custommapbox
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashScreen : AppCompatActivity() {
+
+    private var mDelayHandler: Handler? = null
+    private var SPLASH_DELAY: Long = 2000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        mDelayHandler = Handler()
+        mDelayHandler!!.postDelayed(mRunnable, SPLASH_DELAY)
+    }
+
+    internal val mRunnable: Runnable = Runnable {
+        if (!isFinishing) {
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+            finish()
+
+        }
+    }
+
+    public override fun onDestroy() {
+        if (mDelayHandler != null) {
+            mDelayHandler!!.removeCallbacks(mRunnable)
+        }
+        super.onDestroy()
+    }
+}
